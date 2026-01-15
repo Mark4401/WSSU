dcedea2bbe22e6b84cd948a7e071382f97d244bf
Mark4401 <194540937+Mark4401@users.noreply.github.com>
2026-01-15 14:12:29 -0600

    The main loops checks if P_Protical.Program_initialized_Code is set to PROG_INIT_FAILURE
    in-case either other guards field to catch DWM edit option or if the extend function was called.
    
    but if the program creates more than 1 window and one is closed either by the system or user action all will closed and the program will exit.
    
    a but i just found.

--- DevEnv/App/Source/Main_Client.cpp
+++ DevEnv/App/Source/Main_Client.cpp
@@ -7,25 +7,30 @@ int main(int argv, char* argc[])
 {
     cout << "\ncalled from an C++ file! :p\n\n";
 
-    Init_Program(DPI_AWARENESS_FALSE, ENABLE_DWM_EDIT);
+    Init_Program(DPI_AWARENESS_FALSE, DISABLE_DWM_EDIT);
     
-    Extend_Outer_Window_Borders(400, 300, 200, 100);
+    //Extend_Outer_Window_Borders(400, 300, 200, 100);
     
     Init_Window(L"Test_Window! ;P | WSML i.e (WIndowing & State Management Library)",
         1920, 1080, true, CENTER);  
 
     Init_Window(L"Test_Window!",
         400, 400, true, POS_TOP_LEFT);
+
+    Init_Window(L"Test_Window! 2",
+        400, 400, true, POS_TOP_LEFT);
     
     Process_Lists();
 
-    Window window[2];
+    Window window[3];
 
     window[0].Address = Return_Window_Adress(L"Test_Window! ;P | WSML i.e (WIndowing & State Management Library)");
     window[1].Address = Return_Window_Adress(L"Test_Window!");
+    window[2].Address = Return_Window_Adress(L"Test_Window! 2");
 
     cout << "\n" << window[0].Address << "\n"
-        << window[1].Address << "\n";
+        << window[1].Address << "\n"
+        << window[2].Address << "\n";
 
     while(System_Queue())
     {
